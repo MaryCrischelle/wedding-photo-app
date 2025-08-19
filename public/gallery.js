88856bb9ba6c7ed35d8bcb555a50003397fa3047
Your Name <user@example.com>
2025-08-19 22:55:42 +0800

    Make gallery photos downloadable

--- public/gallery.js
+++ public/gallery.js
@@ -4,10 +4,15 @@ fetch('/api/gallery')
   .then(data => {
     if (data.images && data.images.length) {
       data.images.forEach(url => {
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = url.split('/').pop();
+        link.className = 'block';
         const img = document.createElement('img');
         img.src = url;
         img.className = 'w-full h-32 object-cover rounded-lg';
-        galleryGrid.appendChild(img);
+        link.appendChild(img);
+        galleryGrid.appendChild(link);
       });
     } else {
       galleryGrid.innerHTML = '<p class="col-span-4 text-blue-700">No photos uploaded yet.</p>';
